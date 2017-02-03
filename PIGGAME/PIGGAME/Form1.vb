db8386883e25437ebf93e3a6305e046c391df07b
304830199 <304830199@E154039>
2017-02-03 10:00:04 -0600

    LISTO BOTON PARAR

--- PIGGAME/PIGGAME/Form1.vb
+++ PIGGAME/PIGGAME/Form1.vb
@@ -1,5 +1,6 @@
 ﻿Public Class Form1
     Dim Parcial As Integer = 0
+    Dim Jugador As Integer = 0
     Private Sub btnTirar_Click(sender As Object, e As EventArgs) Handles btnTirar.Click
         Dim dado As Integer = Random()
         txtDado.Text = dado
@@ -7,6 +8,8 @@
         txtPuntajeParcial.Text = Parcial
         If dado = 1 Then
             Parcial = 0
+            Jugador = 0
+            txtJugador.Text = 0
             txtPuntajeParcial.Text = 0
             txtDado.Text = ""
         End If
@@ -16,4 +19,10 @@
         Dim valor As Integer = Int((20 * Rnd()) + 1)
         Return valor
     End Function
+
+    Private Sub btnParar_Click(sender As Object, e As EventArgs) Handles btnParar.Click
+        Jugador += Parcial
+        txtJugador.Text = Jugador
+        MessageBox.Show("TURNO DE LA MAQUINA", "My Application", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+    End Sub
 End Class
