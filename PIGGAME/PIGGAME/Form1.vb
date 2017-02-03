7880af65a392cd45f519724407293203835a74d0
304830199 <304830199@E154039>
2017-02-03 09:49:06 -0600

    Listo If para cuando dado = 1 , borra el puntaje parcial

--- PIGGAME/PIGGAME/Form1.vb
+++ PIGGAME/PIGGAME/Form1.vb
@@ -1,7 +1,15 @@
 ﻿Public Class Form1
+    Dim Parcial As Integer = 0
     Private Sub btnTirar_Click(sender As Object, e As EventArgs) Handles btnTirar.Click
         Dim dado As Integer = Random()
         txtDado.Text = dado
+        Parcial += dado
+        txtPuntajeParcial.Text = Parcial
+        If dado = 1 Then
+            Parcial = 0
+            txtPuntajeParcial.Text = 0
+            txtDado.Text = ""
+        End If
     End Sub
 
     Function Random()
